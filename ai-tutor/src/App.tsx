acca9495a59053e5c7e5bd880b7d43d800462cad
Wayne <wguo6358@gmail.com>
2024-02-19 03:59:54 +0000

    finished

--- ai-tutor/src/App.tsx
+++ ai-tutor/src/App.tsx
@@ -21,22 +21,23 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
-
-setupIonicReact({mode:"ios"});
+import {MathJaxContext} from 'better-react-mathjax'
+setupIonicReact({ mode: "ios" });
 
 const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route exact path="/home">
-          <Home />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
-      </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp>
+  <MathJaxContext>
+    <IonApp>
+      <IonReactRouter>
+        <IonRouterOutlet>
+          <Route exact path="/home">
+            <Home />
+          </Route>
+          <Route exact path="/">
+            <Redirect to="/home" />
+          </Route>
+        </IonRouterOutlet>
+      </IonReactRouter>
+    </IonApp></MathJaxContext>
 );
 
 export default App;
--- ai-tutor/src/pages/Home.tsx
+++ ai-tutor/src/pages/Home.tsx
@@ -1,15 +1,32 @@
-import { IonButton, IonCol, IonContent, IonGrid, IonHeader, IonItem, IonList, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonCheckbox, IonCol, IonContent, IonGrid, IonHeader, IonInput, IonItem, IonList, IonPage, IonProgressBar, IonRow, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 import { Tldraw } from '@tldraw/tldraw';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import OpenAI from "openai";
+import * as htmlToImage from 'html-to-image';
+import { MathJax } from 'better-react-mathjax';
+import Markdown from 'react-markdown'
 
-const openai = new OpenAI({apiKey:, dangerouslyAllowBrowser: true});
 
 const Home: React.FC = () => {
   const upload = useRef(null)
   const [files, setFiles] = useState([])
+  const openai = useRef(null)
+  const [status, setStatus] = useState("")
+  const [question, setQuestion] = useState("")
+  useEffect(() => {
+    var key = localStorage.getItem("key")
+    if (!key) {
+      key = prompt("Key")
+      localStorage.setItem("key", key)
+    }
+    openai.current = new OpenAI({ apiKey: key, dangerouslyAllowBrowser: true });
+  }, [])
+  useEffect(() => {
+    console.log(question)
+  }, [question])
+  const thread = useRef(null)
   return (
     <IonPage>
       <IonHeader>
@@ -20,31 +37,134 @@ const Home: React.FC = () => {
       <IonContent>
         <IonGrid style={{ height: "100%" }}>
           <IonRow style={{ height: "100%" }}>
-            <IonCol>
-              Upload files to start, you can upload course slides, practise questions, assignments, etc.
-              <br />
-              
-              <IonButton onClick={()=>{
-                //@ts-ignore
-                upload.current.click()
-              }}>Upload Files</IonButton>
-              <input onChange={(e)=>{ 
-                setFiles([...e.target.files])
-              }} type="file" multiple={true} ref={upload} hidden></input>
-              <IonList>
-                {
-                  files.map((x, index)=>(
-                    <IonItem key={index}>{x.name}</IonItem>
-                  ))
-                }
-              </IonList>
-              {
-                files.length==0 || <IonButton expand='block'>Go</IonButton>
-              }
-            </IonCol>
-            <IonCol style={{ height: "100%" }}>
+            {
+              question == "" ?
+                <>
+                  <IonCol>
+                    Upload files to start, you can upload course slides, practise questions, assignments, etc.
+                    <br />
+
+                    <IonButton onClick={() => {
+                      //@ts-ignore
+                      upload.current.click()
+                    }}>Upload Files</IonButton>
+                    <input onChange={(e) => {
+                      setFiles([...e.target.files])
+                    }} type="file" accept="image/*,.pdf,.docx" multiple={true} ref={upload} hidden></input>
+                    <IonList>
+                      {
+                        files.map((x, index) => (
+                          <IonItem key={index}>{x.name}</IonItem>
+                        ))
+                      }
+                    </IonList>
+                    <IonProgressBar type="indeterminate" style={{ visibility: status != "in_progress" ? "hidden" : "visible" }}></IonProgressBar>
+                    {
+                      files.length == 0 || <IonButton expand='block' onClick={async () => {
+                        setStatus("in_progress")
+                        const reader = new FileReader();
+                        const file_ids = []
+                        await Promise.all(files.map(async (x, index) => {
+                          const file = await openai.current.files.create({
+                            file: x,
+                            purpose: "assistants",
+                          });
+                          file_ids.push(file.id)
+                        }))
+                        console.log(file_ids.length)
+                        thread.current = await openai.current.beta.threads.create({
+                          messages: [
+                            {
+                              "role": "user",
+                              "content": "You are an AI tutor, user will upload their study material and you will give more practise questions. You should give multiple choice or short response questions. You give one question at a time, you should not give answer. You will give questions that are similar, do not ask back just give questions. Do not format your response using markdown, instead, use LaTex",
+                              "file_ids": file_ids
+                            }
+                          ]
+                        });
+                        const run = await openai.current.beta.threads.runs.create(
+                          thread.current.id,
+                          { assistant_id: "asst_NRPo16m74furPJyI6c33IOYj" }
+                        );
+                        var intId = setInterval(async () => {
+                          const res = await openai.current.beta.threads.runs.retrieve(
+                            thread.current.id,
+                            run.id
+                          );
+                          console.log(res.status)
+                          if (res.status == "completed") {
+                            clearInterval(intId)
+                            const threadMessages = await openai.current.beta.threads.messages.list(thread.current.id)
+                            setQuestion(threadMessages.data[0].content[0].text)
+                          }
+                          setStatus(res.status)
+                        }, 1000)
+                      }}>Go</IonButton>
+                    }
+                  </IonCol></> : <>
+                  <IonCol style={{ fontSize: "30px" }}>
+                    <MathJax>
+                      {question.value}
+                    </MathJax>
+                    <IonInput id="ans" placeholder="Input your answer here"></IonInput>
+                    <br />
+                    <IonCheckbox id="screensot">Submit my scratch with my answer</IonCheckbox>
+                    <br />
+                    <IonButton onClick={async () => {
+                      var node = document.getElementById('1');
+                      htmlToImage.toBlob(node)
+                        .then(async function (blob) {
+                          console.log(blob)
+                          const myfile = await openai.current.files.create({
+                            file: new File([blob],"screenshot.png"),
+                            purpose: "assistants",
+
+                          });
+                          console.log(myfile)
+                          const threadMessages = await openai.current.beta.threads.messages.create(
+                            thread.current.id,
+                            { role: "user", content: "User said: " + document.getElementById("ans").value+". Talk about user's answer then move to next question.", "file_ids": [myfile.id] }
+                          );
+                          const run = await openai.current.beta.threads.runs.create(
+                            thread.current.id,
+                            { assistant_id: "asst_NRPo16m74furPJyI6c33IOYj" }
+                          );
+                          var intId = setInterval(async () => {
+                            const res = await openai.current.beta.threads.runs.retrieve(
+                              thread.current.id,
+                              run.id
+                            );
+                            console.log(res.status)
+                            if (res.status == "completed") {
+                              clearInterval(intId)
+                              const threadMessages = await openai.current.beta.threads.messages.list(thread.current.id)
+                              setQuestion(threadMessages.data[0].content[0].text)
+                            }
+                            setStatus(res.status)
+                          }, 1000)
+                        })
+                        // .catch(function (error) {
+                        //   console.error('oops, something went wrong!', error);
+                        // });
+
+
+                    }}>Submit</IonButton>
+                  </IonCol>
+                </>
+            }
+            <IonCol id="1" style={{ height: "100%" }}>
               Scratch Paper
               <Tldraw />
+              <button onClick={() => {
+                var node = document.getElementById('1');
+
+                htmlToImage.toPng(node)
+                  .then(function (dataUrl) {
+                    console.log(dataUrl)
+                  })
+                  .catch(function (error) {
+                    console.error('oops, something went wrong!', error);
+                  });
+              }}>screenshot</button>
             </IonCol>
           </IonRow>
         </IonGrid>
